2c5e505b8f2a59ee2a057f287ff535314adb1c92
maatique <mohid.ali@invozone.com>
2023-05-23 11:12:47 +0000

    first commit

---
+++ .github/workflows/test_script.py
@@ -0,0 +1,10 @@
+def add_numbers(a, b):
+    return a + b
+
+def test_add_numbers():
+    assert add_numbers(2, 3) == 5
+    assert add_numbers(10, -5) == 5
+    assert add_numbers(0, 0) == 0
+    print("All tests passed successfully!")
+
+test_add_numbers()
